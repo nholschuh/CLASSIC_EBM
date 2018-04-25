22758e84caffd938ab0b15fa475ed15bf236da28
jraylmer <32389493+jraylmer@users.noreply.github.com>
2018-04-25 16:09:26 +0100

    (Bug fix) Heat transports now use Q(xi) rather than Q0

--- bin/heattransports.py
+++ bin/heattransports.py
@@ -14,22 +14,23 @@ from src import parameters as pm, analytics as an
 from src import plotting as pl, fileIO
 
 
-def main(xi=0.9, smooth_coalbedo=False):
+def main(xi=0.9, smooth_coalbedo=False, latitude_axis=False):
     
-    x = np.arange(0.0, 1.001, 0.01)
+    x = np.arange(0.0, 1.001, 0.001)
     HT = np.zeros(len(x))
     HFC = np.zeros(len(x))
+    Q = an.Q(xi, smooth_coalbedo=smooth_coalbedo)
     
     print "Using %s-coalbedo..." % ('smoothed' if smooth_coalbedo else 
         'step-function')
     
     for k in xrange(len(x)):
-        HT[k] = an.HeatTransport(x[k], xi, smooth_coalbedo=smooth_coalbedo)
+        HT[k] =an.HeatTransport(x[k], xi, Q=Q, smooth_coalbedo=smooth_coalbedo)
         HFC[k] = an.HeatFluxConvergence(
-            x[k], xi, smooth_coalbedo=smooth_coalbedo)
+            x[k], xi, Q=Q, smooth_coalbedo=smooth_coalbedo)
     
-    fig1, ax1 = pl.PlotHeatTransport(x, HT, xi)
-    fig2, ax2 = pl.PlotHeatFluxConvergence(x, HFC, xi)
+    fig1, ax1 = pl.PlotHeatTransport(x, HT, xi, latitude_axis)
+    fig2, ax2 = pl.PlotHeatFluxConvergence(x, HFC, xi, latitude_axis)
     subdir_name = 'StandardHeatTransports'+('_SmoothCoalbedo'*smooth_coalbedo)
     fileIO.SaveFigures([fig1, fig2], subdir_name)
     fileIO.SaveFigures([fig1, fig2], subdir_name, '.svg')
@@ -41,4 +42,5 @@ def main(xi=0.9, smooth_coalbedo=False):
 
 if __name__ == '__main__':
     pl.SetRCParams()
-    main(smooth_coalbedo=('smooth_coalbedo' in sys.argv))
+    main(smooth_coalbedo=('smooth_coalbedo' in sys.argv),
+        latitude_axis=('latitude_axis' in sys.argv))
